a8c0f8d2a46fc2d6ff1473bb5e1524beefe220e0
hunter <guyuesh2@gmail.com>
2021-08-02 20:28:51 +0800

    配置 gitignore,删除 dist 添加 dist ignore

--- dev_env/src/index.js
+++ dev_env/src/index.js
@@ -1,10 +1,11 @@
+import _ from 'lodash';
 import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
   const btn = document.createElement('button');
 
-  element.innerHTML = ['Hello', 'webpack'].join(" ");
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
