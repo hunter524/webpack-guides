4c3a2817a6de051e70d29f90c823cd8ebd276f03
hunter <guyuesh2@gmail.com>
2021-09-07 20:04:54 +0800

    添加 webpack config 配置的多种方式 添加 creat-react-app 的配置方式

--- simple-css-loader/webpackfunc.config.js
+++ simple-css-loader/webpackfunc.config.js
@@ -1,9 +1,8 @@
 //通过返回一个函数，由函数动态的返回config 对象的配置
-module.exports = function (env = {},args) {
+// env 表示当前环境变量的一个 Object
+// args 也表示为一个 Object 的环境变量
+module.exports = function (env = {}, args) {
     console.log(JSON.stringify(env))
-    let array = new Array(args);
-    array.forEach(value => {
-        console.log(`value ${JSON.stringify(value)}`)
-    })
-    return{}
+    console.log(`value ${JSON.stringify(args)}`)
+    return {}
 }
